15863165a1b1e73b0e8088f88179846e75d6428d
Sandro Ricardo De Souza <sandro.fisica@gmail.com>
2017-04-24 11:25:55 -0300

    Adding project map

---
+++ Project_MM.mm
@@ -0,0 +1,128 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="MM3" LOCALIZED_STYLE_REF="default" FOLDED="false" ID="ID_769428002" CREATED="1492534445500" MODIFIED="1493043566640" BACKGROUND_COLOR="#66ccff"><hook NAME="MapStyle">
+    <properties show_icon_for_attributes="true" fit_to_viewport="false;" show_note_icons="true"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="21" RULE="ON_BRANCH_CREATION"/>
+<edge WIDTH="4"/>
+<node TEXT="data" LOCALIZED_STYLE_REF="default" POSITION="left" ID="ID_1195417951" CREATED="1492538255608" MODIFIED="1493043546728" HGAP_QUANTITY="16.249999932944778 pt" VSHIFT_QUANTITY="-8.249999754130847 pt">
+<edge COLOR="#7c007c" WIDTH="4"/>
+<cloud COLOR="#ccffcc" SHAPE="ROUND_RECT"/>
+<node TEXT="" ID="ID_1498376623" CREATED="1493043730726" MODIFIED="1493043730726">
+<node TEXT="raw_data" ID="ID_1705173546" CREATED="1493043591183" MODIFIED="1493043623264">
+<node TEXT="2017-04-23-srds-mercury.csv" ID="ID_1554936895" CREATED="1493043624287" MODIFIED="1493043631192"/>
+<node TEXT="2017-04-23-srds-planets.csv" ID="ID_1451258287" CREATED="1493043670591" MODIFIED="1493043684536"/>
+</node>
+</node>
+</node>
+<node TEXT="deliver" LOCALIZED_STYLE_REF="default" POSITION="right" ID="ID_1597477379" CREATED="1492540559829" MODIFIED="1493043555251" HGAP_QUANTITY="13.999999999999996 pt" VSHIFT_QUANTITY="-8.999999731779102 pt">
+<edge COLOR="#00ffff" WIDTH="4"/>
+<cloud COLOR="#ffcccc" SHAPE="ROUND_RECT"/>
+</node>
+<node TEXT="develop" LOCALIZED_STYLE_REF="default" POSITION="right" ID="ID_1944220593" CREATED="1493043223223" MODIFIED="1493043564369">
+<edge COLOR="#00ffff" WIDTH="4"/>
+<cloud COLOR="#ffcccc" SHAPE="ROUND_RECT"/>
+<node TEXT="2017-04-23-srds-create-directory-tree.ipynb" LOCALIZED_STYLE_REF="default" ID="ID_1058030262" CREATED="1493043365663" MODIFIED="1493043494806">
+<icon BUILTIN="unchecked"/>
+<edge COLOR="#00ffff" WIDTH="4"/>
+<cloud COLOR="#ffcccc" SHAPE="ROUND_RECT"/>
+</node>
+</node>
+<node TEXT="README.md" POSITION="left" ID="ID_522858621" CREATED="1493043771046" MODIFIED="1493043782376">
+<edge COLOR="#7c007c"/>
+</node>
+<node TEXT="MM3.mm" POSITION="right" ID="ID_1748243366" CREATED="1493043796494" MODIFIED="1493043857873">
+<edge COLOR="#007c7c"/>
+<richcontent TYPE="DETAILS" HIDDEN="true">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Mapa do projeto
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</map>
